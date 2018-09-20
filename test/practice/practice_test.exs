f9a6259dcc243ebee7ff2176cef7df3b804bc1db
Emily Miller-McGlone <emilymiller-mcglone@Emilys-MacBook-Pro.local>
2018-09-19 21:49:12 -0400

    fix tests

--- test/practice/practice_test.exs
+++ test/practice/practice_test.exs
@@ -10,26 +10,27 @@ defmodule Practice.PracticeTest do
   end
 
   test "factor some numbers" do
-    assert factor(5) == [5]
-    assert factor(8) == [2, 2, 2]
-    assert factor(12) == [2, 2, 3]
-    assert factor(226_037_113) == [3449, 65537]
-    assert factor(1575) == [3, 3, 5, 5, 7]
+    assert factor("5") == "[5]"
+    assert factor("8") == "[2, 2, 2, 1]"
+    assert factor("12") == "[2, 2, 3]"
+    assert factor("226037113") == "[3449, 65537]"
+    assert factor("1575") == "[3, 3, 5, 5, 7]"
   end
 
   test "evaluate some expressions" do
-    assert calc("5") == 5
-    assert calc("5 + 1") == 6
-    assert calc("5 * 3") == 15
-    assert calc("10 / 2") == 5
-    assert calc("10 - 2") == 8
-    assert calc("5 * 3 + 8") == 23
-    assert calc("8 + 5 * 3") == 23
+    assert calc("5") == "5"
+    assert calc("5 + 1") == "6"
+    assert calc("5 * 3") == "15"
+    assert calc("10 / 2") == "5"
+    assert calc("10 - 2") == "8"
+    assert calc("5 * 3 + 8") == "23"
+    assert calc("8 + 5 * 3") == "23"
   end
 
   # TODO: Add two unit tests for palindrome.
   test "check if words are palindromes" do
     assert palindrome("hannah") == true
+    assert palindrome("Hannah") == false
     assert palindrome("party") == false
   end
 end
